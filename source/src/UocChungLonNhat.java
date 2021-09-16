a63987a501944260902c10549ed60cf9deed8db4
ThaiVu54 <thai01654369440@gmail.com>
2021-09-17 00:28:22 +0700

    greatest common divisor

---
+++ source/src/UocChungLonNhat.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class UocChungLonNhat {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter first number: ");
+        a = sc.nextInt();
+        System.out.print("enter second number: ");
+        b = sc.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if(a == 0 || b == 0){
+            System.out.println("no greatest common factor");
+        }
+        while (a != b){
+            if(a > b){
+                a = a - b;
+            }else b = b - a;
+        }
+        System.out.println("Greatest common factor: " + a);
+    }
+}
